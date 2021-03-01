28cc38e541bc552dcc58bfb3b9ef34d1bae61f07
Chris Canal <chriscanal4@gmail.com>
2021-02-28 20:14:19 -0600

    updated stam.py to be pep 8 compliant

--- stam.py
+++ stam.py
@@ -1,39 +1,30 @@
-import os
 import sys
 import discord
-import sentience
 import traceback
 import unicodedata
-from dotenv import load_dotenv
-from config import required_environment_variables
+from modules.reply import Reply
+from config import discord_token
+from modules.module import Module
+from utilities import client, utils
+from modules.questions import QQManager
+from modules.sentience import sentience
+from modules.videosearch import VideoSearch
+from modules.invitemanager import InviteManager
+from modules.stampcollection import StampsModule
 from datetime import datetime, timezone, timedelta
-from utilities import Utilities, check_environment
-
-load_dotenv()
-
-intents = discord.Intents.default()
-intents.members = True
-client = discord.Client(intents=intents)
-
-check_environment(required_environment_variables)
-
-utils = Utilities.getInstance()
-utils.client = client
-
-from module import Module
-from stampcollection import StampsModule
-from questions import QQManager
-from reply import Reply
-from videosearch import VideoSearch
-from invitemanager import InviteManager
 
 
 @client.event
 async def on_ready():
     print(f"{client.user} has connected to Discord!")
-    guild = discord.utils.find(lambda g: g.name == guildname, client.guilds)
+    print("searching for a guild named '%s'" % utils.GUILD)
+    print(client.guilds)
+    guild = discord.utils.get(client.guilds, name=utils.GUILD)
 
-    print(guild.id, guild.name)
+    if guild is None:
+        raise Exception("Guild Not Found : '%s'" % utils.GUILD)
+
+    print("found a guild named '%s' with id '%s'" % (guild.name, guild.id))
 
     members = "\n - ".join([member.name for member in guild.members])
     print(f"Guild Members:\n - {members}")
@@ -52,7 +43,7 @@ async def on_message(message):
 
     if hasattr(message.channel, "name") and message.channel.name == "general":
         print("Last message was no longer us")
-        utils.lastmessagewasYTquestion = False
+        utils.last_message_was_youtube_question = False
 
     if message.content == "bot test":
         response = "I'm alive!"
@@ -65,15 +56,16 @@ async def on_message(message):
             reference = await message.channel.fetch_message(
                 message.reference.message_id
             )
-            reftext = reference.content
-            replyURL = reftext.split("\n")[-1].strip()
+            reference_text = reference.content
+            reply_url = reference_text.split("\n")[-1].strip()
 
             response = 'This is a reply to message %s:\n"%s"' % (
                 message.reference.message_id,
-                reftext,
+                reference_text,
             )
             response += (
-                'which should be taken as an answer to the question at: "%s"' % replyURL
+                'which should be taken as an answer to the question at: "%s"'
+                % reply_url
             )
         else:
             response = "This is not a reply"
@@ -84,62 +76,22 @@ async def on_message(message):
     ):
         print("[resetting can-invite roles]")
         await message.channel.send("[resetting can-invite roles, please wait]")
-        guild = discord.utils.find(lambda g: g.name == guildname, client.guilds)
-        print(guildname, guild)
+        guild = discord.utils.find(lambda g: g.name == utils.GUILD, client.guilds)
+        print(utils.GUILD, guild)
         role = discord.utils.get(guild.roles, name="can-invite")
         print("there are", len(guild.members), "members")
-        resetuserscount = 0
+        reset_users_count = 0
         for member in guild.members:
             if utils.get_user_stamps(member) > 0:
                 print(member.name, "can invite")
                 await member.add_roles(role)
-                resetuserscount += 1
+                reset_users_count += 1
             else:
                 print(member.name, "has 0 stamps, can't invite")
         await message.channel.send(
-            "[Invite Roles Reset for %s users]" % resetuserscount
+            "[Invite Roles Reset for %s users]" % reset_users_count
         )
         return
-    # elif message.content.lower() == "mh":
-    #   guild = discord.utils.find(lambda g: g.name == guildname, client.guilds)
-    #   #general = discord.utils.find(lambda c: c.name == "general", guild.channels)
-
-    #   with open("stamps.csv", 'w') as stamplog:
-    #       stamplog.write("msgid,type,from,to\n")
-
-    #       for channel in guild.channels:
-    #           print("#### Considering", channel.type, channel.name, "####")
-    #           if channel.type == discord.TextChannel:
-    #               print("#### Logging", channel.name, "####")
-    #               async for message in channel.history(limit=None):
-    #                   # print("###########")
-    #                   # print(message.content[:20])
-    #                   reactions = message.reactions
-    #                   if reactions:
-    #                       # print(reactions)
-    #                       for reaction in reactions:
-    #                           reacttype = getattr(reaction.emoji, 'name', '')
-    #                           if reacttype in ["stamp", "goldstamp"]:
-    #                               # print("STAMP")
-    #                               users = await reaction.users().flatten()
-    #                               for user in users:
-    #                                   string = "%s,%s,%s,%s" % (message.id, reacttype, user.id, message.author.id)
-    #                                   print(string)
-    #                                   stamplog.write(string + "\n")
-    #                                   # print("From", user.id, user)
-    #   return
-
-    # elif message.content.lower() == "invite test" and message.author.name == "robertskmiles":
-    #   guild = discord.utils.find(lambda g: g.name == guildname, client.guilds)
-    #   welcome = discord.utils.find(lambda c: c.name == "welcome", guild.channels)
-    #   invite = await welcome.create_invite(max_uses=1,
-    #                                       temporary=True,
-    #                                       unique=True,
-    #                                       reason="Requested by %s" % message.author.name)
-    #   print(invite)
-    #   await message.channel.send(invite.url)
-
-    result = None
 
     # What are the options for responding to this message?
     # Prepopulate with a dummy module, with 0 confidence about its proposed response of ""
@@ -164,24 +116,19 @@ async def on_message(message):
 
     if not result:  # no results from the modules, try the sentience core
         try:
-            result = sentience.processMessage(message, client)
+            confidence, result = sentience.processMessage(message, client)
         except Exception as e:
             if hasattr(message.channel, "name") and message.channel.name in (
                 "bot-dev",
                 "bot-dev-priv",
                 "181142785259208704",
             ):
-                try:
-                    errortype = sentience.dereference(
-                        "{{$errorType}}"
-                    )  # grab a random error type from the factoid db
-                except:
-                    errortype = "SeriousError"  # if the dereference failed, it's bad
+                error_type = "SeriousError"  # if the dereference failed, it's bad
                 x = sys.exc_info()[2].tb_next
                 print(e, type(e))
                 traceback.print_tb(x)
                 lineno = sys.exc_info()[2].tb_next.tb_lineno
-                result = "%s %s: %s" % (errortype, lineno, str(e))
+                result = "%s %s: %s" % (error_type, lineno, str(e))
             else:
                 x = sys.exc_info()[2].tb_next
                 print(e, type(e))
@@ -197,60 +144,64 @@ async def on_message(message):
 async def on_socket_raw_receive(msg):
     """This event fires whenever basically anything at all happens.
     Anyone joining, leaving, sending anything, even typing and not sending...
-    So I'm going to use it as a kind of 'update' or 'tick' function, for things the bot needs to do regularly. Yes this is hacky.
+    So I'm going to use it as a kind of 'update' or 'tick' function,
+    for things the bot needs to do regularly. Yes this is hacky.
     Rate limit these things, because this function might be firing a lot"""
 
     # never fire more than once a second
 
-    tickcooldown = timedelta(seconds=1)
+    tick_cool_down = timedelta(seconds=1)
     now = datetime.now(timezone.utc)
 
-    if (now - utils.lasttickts) > tickcooldown:
+    if (now - utils.last_timestamp) > tick_cool_down:
         print("|", end="")
-        # print("last message was yt question?:", lastmessagewasYTquestion)
-        utils.lasttickts = now
+        utils.last_timestamp = now
     else:
         print(".", end="")
         return
 
     # check for new youtube comments
-    newcomments = utils.check_for_new_youtube_comments()
-    if newcomments:
-        for comment in newcomments:
+    new_comments = utils.check_for_new_youtube_comments()
+    if new_comments:
+        for comment in new_comments:
             if "?" in comment["text"]:
-                utils.addQuestion(comment)
-    # with open("qq.json", 'w') as qqfile:  # we modified the queue, put it in a file to persist
-    # 	json.dump(qq, qqfile, indent="\t")
-    qq = utils.getNextQuestion("rowid")
+                utils.add_question(comment)
+    qq = utils.get_next_question("rowid")
     if qq:
         # ask a new question if it's been long enough since we last asked one
-        qaskcooldown = timedelta(hours=6)
+        question_ask_cooldown = timedelta(hours=6)
 
-        if (now - utils.lastqaskts) > qaskcooldown:
+        if (now - utils.last_question_asked_timestamp) > question_ask_cooldown:
             if (
-                not utils.lastmessagewasYTquestion
+                not utils.last_message_was_youtube_question
             ):  # Don't ask anything if the last thing posted in the chat was us asking a question
-                utils.lastqaskts = now
+                utils.last_question_asked_timestamp = now
                 report = utils.get_latest_question()
-                guild = discord.utils.find(lambda g: g.name == guildname, client.guilds)
+                guild = discord.utils.find(
+                    lambda g: g.name == utils.GUILD, client.guilds
+                )
                 general = discord.utils.find(
                     lambda c: c.name == "general", guild.channels
                 )
                 await general.send(report)
-                utils.lastmessagewasYTquestion = True
+                utils.last_message_was_youtube_question = True
             else:
-                utils.lastqaskts = now  # wait the full time again
+                utils.last_question_asked_timestamp = now  # wait the full time again
                 print(
                     "Would have asked a question, but the last post in the channel was a question we asked. So we wait"
                 )
         else:
             print(
                 "%s Questions in queue, waiting %s to post"
-                % (len(qq), str(qaskcooldown - (now - utils.lastqaskts)))
+                % (
+                    len(qq),
+                    str(
+                        question_ask_cooldown
+                        - (now - utils.last_question_asked_timestamp)
+                    ),
+                )
             )
             return
-
-    # await message.channel.send(result)
 
 
 @client.event
@@ -275,62 +226,37 @@ async def on_raw_reaction_remove(payload):
         await module.processRawReactionEvent(payload, client)
 
 
-# result = None
-
-# # What are the options for responding to this message?
-# # Prepopulate with a dummy module, with 0 confidence about its proposed response of ""
-# options = [(Module(), 0, "")]
-
-# for module in modules:
-# 	print("Asking module: %s" % str(module))
-# 	output = module.canProcessReaction(payload, client)
-# 	print("output is", output)
-# 	confidence, result = output
-# 	if confidence > 0:
-# 		options.append((module, confidence, result))
-
-# # Go with whichever module was most confident in its response
-# options = sorted(options, key=(lambda o: o[1]), reverse=True)
-# print(options)
-# module, confidence, result = options[0]
-
-# if confidence > 0:  # if the module had some confidence it could reply
-# 	if not result:  # but didn't reply in canProcessMessage()
-# 		confidence, result = await module.processReactionEvent(payload, client)
-
-
 if __name__ == "__main__":
     # when was the most recent comment we saw posted?
-    utils.latestcommentts = datetime.now(timezone.utc)  # - timedelta(hours=8)
+    utils.latest_comment_timestamp = datetime.now(timezone.utc)
 
     # when did we last hit the API to check for comments?
-    utils.lastcheckts = datetime.now(timezone.utc)
+    utils.last_check_timestamp = datetime.now(timezone.utc)
 
     # how many seconds should we wait before we can hit YT API again
     # this the start value. It doubles every time we don't find anything new
-    utils.ytcooldown = utils.tds(60)
+    utils.youtube_cool_down = timedelta(seconds=60)
 
     # timestamp of when we last ran the tick function
-    utils.lasttickts = datetime.now(timezone.utc)
+    utils.last_timestamp = datetime.now(timezone.utc)
 
     # timestamp of last time we asked a youtube question
-    utils.lastqaskts = datetime.now(timezone.utc)
-
-    guildname = utils.GUILD
+    utils.last_question_asked_timestamp = datetime.now(timezone.utc)
 
     # Was the last message posted in #general by anyone, us asking a question from YouTube?
-    utils.lastmessagewasYTquestion = (
+    utils.last_message_was_youtube_question = (
         True  # We start off not knowing, but it's better to assume yes than no
     )
 
-    utils.modulesdict = {
+    utils.modules_dict = {
         "StampsModule": StampsModule(),
         "QQManager": QQManager(),
         "VideoSearch": VideoSearch(),
         "Reply": Reply(),
         "InviteManager": InviteManager(),
+        "Sentience": sentience,
     }
 
-    modules = utils.modulesdict.values()
+    modules = utils.modules_dict.values()
 
-    client.run(utils.TOKEN)
+    client.run(discord_token)
