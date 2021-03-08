672c5bce5637f95e61f0a921ceb7cb352303761e
Chris Canal <chriscanal4@gmail.com>
2021-03-08 01:22:27 -0600

    added development instructions and demo to readme

--- stam.py
+++ stam.py
@@ -32,6 +32,9 @@ async def on_ready():
 
 @client.event
 async def on_message(message):
+    if message.content == "Hello Stampy":
+        await message.channel.send("Hello Chris")
+
     # don't react to our own messages
     if message.author == client.user:
         return
