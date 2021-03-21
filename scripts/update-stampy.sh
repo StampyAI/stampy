e633e395edaf5f4020c8bff57ab1be0e503a6875
Chris Canal <chriscanal4@gmail.com>
2021-03-21 16:53:10 -0500

    updatd to write to log file

--- scripts/update-stampy.sh
+++ scripts/update-stampy.sh
@@ -2,7 +2,8 @@ pkill -f stam.py
 cd ~/stampy
 python3 -m scripts.notify-discord-stampy-offline
 git pull
+conda deactivate
 conda env remove -n stampy
 conda env create -f environment.yml
 conda activate stampy
-python3 stam.py
+./runstampy > ~/"stampy-log-$(date +"%F-%T")" 2>&1 &
