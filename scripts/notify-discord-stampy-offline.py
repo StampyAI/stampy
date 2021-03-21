4b3f22d40c94bc6881b7f583c7aa39c848269ff1
Chris Canal <chriscanal4@gmail.com>
2021-03-21 00:22:42 -0500

    added script to notify discord users if stampy is down

--- scripts/notify-discord-stampy-offline.py
+++ scripts/notify-discord-stampy-offline.py
@@ -1,16 +1,15 @@
 import discord
-import asyncio
-
+from git import Repo
 from config import ENVIRONMENT_TYPE, bot_dev_channels, discord_token
-
-from discord.ext.commands import Bot
-
-bot = Bot(command_prefix="!")
-
-import discord
-import asyncio
+from datetime import datetime
 
 client = discord.Client()
+
+offline_message = (
+    "I'm going offline for maintenance. %s is updating me.\n"
+    + "This is their latest commit message that I've received: \n%s\n"
+    + "This message was committed at %s"
+)
 
 
 @client.event
@@ -18,22 +17,17 @@ async def on_ready():
     print("Logged in as")
     print(client.user.name)
     print(client.user.id)
-    await client.get_channel(bot_dev_channels[ENVIRONMENT_TYPE]).send("A new stampy is dead")
+    repo = Repo(".")
+    master = repo.head.reference
+    git_log = master.log()[-1]
+    actor = git_log.actor.name
+    git_message = git_log.message
+    date = datetime.fromtimestamp(git_log.time[0]).strftime("%A, %B %d, %Y at %I:%M:%S %p UTC")
+    message = offline_message % (actor, git_message, date)
+    print(message)
+    await client.get_channel(bot_dev_channels[ENVIRONMENT_TYPE]).send(message)
     print("------")
     exit()
 
 
 client.run(discord_token)
-
-#
-# @bot.command()
-# async def send(message):
-#     await bot.get_channel(803448149946662923).send(message)
-#     # await bot.get_channel(bot_dev_channels[ENVIRONMENT_TYPE]).send(message)
-#
-#
-# async def main():
-#     await send("random message")
-#
-#
-# asyncio.run(main())
