1b179736d9628b5985cea0b04c900d5e7433000a
Chris Canal <chriscanal4@gmail.com>
2021-03-21 17:33:16 -0500

    updated stampy deployment to use deployment script

--- scripts/notify-discord-stampy-offline.py
+++ scripts/notify-discord-stampy-offline.py
@@ -1,14 +1,13 @@
 import discord
 from git import Repo
 from config import ENVIRONMENT_TYPE, bot_dev_channels, discord_token
-from datetime import datetime
 
 client = discord.Client()
 
 offline_message = (
     "I'm going offline for maintenance. %s is updating me.\n"
-    + "This is their latest commit message that I've received: \n%s\n"
-    + "This message was committed at %s"
+    + "This is their latest commit message that I've received: \n'%s'"
+    + "This message was committed at %s\nI'll be back"
 )
 
 
