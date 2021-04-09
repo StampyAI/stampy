21422926c170023d621b48ee555575d57c33a6c7
= <james.mccuen1@gmail.com>
2021-04-08 22:15:08 -0700

    Small tweaks, testing

--- modules/turry.py
+++ modules/turry.py
@@ -15,7 +15,6 @@ import atexit
 
 
 class Turry(Module):
-
     def __init__(self):
         Module.__init__(self)
         self.dnd_url = "http://api.aidungeon.io/graphql"
@@ -23,52 +22,79 @@ class Turry(Module):
         self.timeout = 1800
         self.token = os.getenv("AI_DUNGEON_TOKEN")
         self.ws = None
-        self.adventure_id = "4c35b5ca-ee26-4894-87ad-1600f4ae8aae"
+        self.adventure_id = "a314f913-7b9e-4777-a48a-b8432e0dceaa"
         self.last_seen_id = None
         atexit.register(self.exit_handler)
         return
 
     def can_process_message(self, message, client=None):
-        if hasattr(message.channel, "name") and message.channel.name == "seed":
+        if hasattr(message.channel, "name") and (
+            message.channel.name == "seed" or message.channel.name == "seed-test"
+        ):
             return 7, ""
         return 0, ""
 
     async def process_message(self, message, client=None):
-        self.ws = await self.get_initialized_websocket()
-        if hasattr(message.channel, "name") and message.channel.name == "seed":
+
+        if hasattr(message.channel, "name") and (
+            message.channel.name == "seed" or message.channel.name == "seed-test"
+        ):
             text = message.clean_content
             match = re.search("^(![^\s]*)(\n|\s|\r|$)", text)
             command = match.group(1)
-            prompt = text.replace(command + ' ', '')
+            prompt = text.replace(command + " ", "")
 
+            if not self.adventure_id:
+                await message.channel.trigger_typing()
+                self.ws = await self.get_initialized_websocket()
+                self.adventure_id = await self.process_game_restart()
 
-            #if command == "!new":
-            #    self.adventure_id = await self.process_game_restart()
-            #    if self.adventure_id:
-            #        response = "New game started: " + self.adventure_id
-            if self.adventure_id:
-                if command == "!do":
-                    message = await self.process_game_next("do", prompt, self.adventure_id, self.last_seen_id)
-                    if message:
-                        response = message[0]
-                        self.last_seen_id = message[1]
-                elif command == "!say":
-                    message = await self.process_game_next("say", prompt, self.adventure_id, self.last_seen_id)
-                    if message:
-                        response = message[0]
-                        self.last_seen_id = message[1]
-                elif command == "!story":
-                    message = await self.process_game_next("story", prompt, self.adventure_id, self.last_seen_id)
-                    if message:
-                        response = message[0]
-                        self.last_seen_id = message[1]
-                elif command == "!start":
-                    message = await self.process_game_next("start", '', self.adventure_id, self.last_seen_id)
+            if command == "!new":
+                await message.channel.trigger_typing()
+                self.ws = await self.get_initialized_websocket()
+                self.adventure_id = await self.process_game_restart()
+                if self.adventure_id:
+                    response = "New game started: " + self.adventure_id
+            elif command == "!do":
+                await message.channel.trigger_typing()
+                self.ws = await self.get_initialized_websocket()
+                message = await self.process_game_next(
+                    "do", prompt, self.adventure_id, self.last_seen_id
+                )
+                if message:
                     response = message[0]
                     self.last_seen_id = message[1]
+            elif command == "!say":
+                await message.channel.trigger_typing()
+                self.ws = await self.get_initialized_websocket()
+                message = await self.process_game_next(
+                    "say", prompt, self.adventure_id, self.last_seen_id
+                )
+                if message:
+                    response = message[0]
+                    self.last_seen_id = message[1]
+            elif command == "!story":
+                await message.channel.trigger_typing()
+                self.ws = await self.get_initialized_websocket()
+                message = await self.process_game_next(
+                    "story", prompt, self.adventure_id, self.last_seen_id
+                )
+                if message:
+                    response = message[0]
+                    self.last_seen_id = message[1]
+            elif command == "!continue":
+                await message.channel.trigger_typing()
+                self.ws = await self.get_initialized_websocket()
+                message = await self.process_game_next(
+                    "start", "", self.adventure_id, self.last_seen_id
+                )
+                response = message[0]
+                self.last_seen_id = message[1]
+            else:
+                return 0, ""
 
-                if response:
-                    return 7, response
+            if response:
+                return 7, response
         """Handle the message, return a string which is your response.
         This is an async function so it can interact with the Discord API if it needs to"""
         return 0, ""
@@ -89,11 +115,15 @@ class Turry(Module):
         return "Turry Module"
 
     async def get_initialized_websocket(self):
-        ws = await websockets.connect(self.ws_url, subprotocols=['graphql-ws'])
-        await ws.send('{"type":"connection_init","payload":{"token":"' + self.token + '"}}')
+        ws = await websockets.connect(self.ws_url, subprotocols=["graphql-ws"])
+        await ws.send(
+            '{"type":"connection_init","payload":{"token":"' + self.token + '"}}'
+        )
         while True:
             resp = await ws.recv()
-            print('Turry Module: Message received during websocket initialization to AI Dungeon:')
+            print(
+                "Turry Module: Message received during websocket initialization to AI Dungeon:"
+            )
             print(resp)
             if json.loads(resp)["type"] == "connection_ack":
                 break
@@ -101,17 +131,21 @@ class Turry(Module):
 
     async def process_game_restart(self):
         ws = self.ws
-        await ws.send('{"id":"1","type":"start","payload":{"variables":{"scenarioId":"1665210","prompt":null},"extensions":{},"operationName":null,"query":"mutation ($scenarioId: String, $prompt: String) {  addAdventure(scenarioId: $scenarioId, prompt: $prompt) {    id    publicId    title    description    musicTheme    tags    nsfw    published    createdAt    updatedAt    deletedAt    publicId    __typename  }}","auth":{"token":"hello"}}}')
-        #await ws.send('{"id":"1","type":"start","payload":{"variables":{},"extensions":{},"query":"subscription {\n  userUpdated {\n    id\n    hasPremium\n    scales\n    __typename\n  }\n}\n","auth":{"token":"hello"}}}')
+        await ws.send(
+            '{"id":"1","type":"start","payload":{"variables":{"scenarioId":"1666826","prompt":null},"extensions":{},"operationName":null,"query":"mutation ($scenarioId: String, $prompt: String) {  addAdventure(scenarioId: $scenarioId, prompt: $prompt) {    id    publicId    title    description    musicTheme    tags    nsfw    published    createdAt    updatedAt    deletedAt    publicId    __typename  }}","auth":{"token":"hello"}}}'
+        )
+        # await ws.send('{"id":"1","type":"start","payload":{"variables":{},"extensions":{},"query":"subscription {"userUpdated": {"id": "hasPremium","scales": "__typename"}}","auth":{"token":"hello"}}}')
         while True:
             resp = await ws.recv()
-            print('Message received during restart():')
+            print("Message received during restart():")
             print(resp)
             json_payload = json.loads(resp)
 
             if json_payload["type"] == "data" and json_payload["id"] == "1":
                 await ws.close()
-                adventure_id_new = json_payload["payload"]["data"]["addAdventure"]["publicId"]
+                adventure_id_new = json_payload["payload"]["data"]["addAdventure"][
+                    "publicId"
+                ]
                 print(adventure_id_new)
                 return adventure_id_new
 
@@ -121,30 +155,52 @@ class Turry(Module):
         print(adventure_id)
 
         # subscribe to current adventure
-        await ws.send('{"id":"1","type":"start","payload":{"variables":{"publicId":"' + str(adventure_id) + '"},"extensions":{},"query":"subscription ($publicId: String) { adventureUpdated(publicId: $publicId) { ...AdventurePlayFragment __typename }}fragment AdventurePlayFragment on Adventure { id playPublicId publicId thirdPerson actionCount lastAction { ...ActionFragment __typename } ...AdventureControllerFragment ...AudioAdventureControllerFragment ...ReviewAdventureControllerFragment __typename}fragment ActionFragment on Action { id text adventureId undoneAt deletedAt __typename}fragment AdventureControllerFragment on Adventure { id actionLoading error gameState events thirdPerson userId characters { id userId name __typename } ...ActionAdventureControllerFragment ...AlterControllerFragment ...QuestControllerFragment ...RememberControllerFragment ...ShareControllerFragment __typename}fragment ActionAdventureControllerFragment on Adventure { id publicId actionCount choices error mode thirdPerson userId worldId characters { id userId name __typename } ...DeathControllerFragment __typename}fragment DeathControllerFragment on Adventure { id publicId mode died __typename}fragment AlterControllerFragment on Adventure { id publicId mode __typename}fragment QuestControllerFragment on Adventure { id publicId lastAction { id text __typename } quests { id text completed active actionGainedId actionCompletedId __typename } __typename}fragment RememberControllerFragment on Adventure { id memory authorsNote __typename}fragment ShareControllerFragment on Adventure { id userId thirdPerson playPublicId characters { id userId name __typename } __typename}fragment AudioAdventureControllerFragment on Adventure { id music lastAction { id text __typename } __typename}fragment ReviewAdventureControllerFragment on Adventure { id actionCount __typename}","auth":{"token":"hello"}}}')
+        await ws.send(
+            '{"id":"1","type":"start","payload":{"variables":{"publicId":"'
+            + str(adventure_id)
+            + '"},"extensions":{},"query":"subscription ($publicId: String) { adventureUpdated(publicId: $publicId) { ...AdventurePlayFragment __typename }}fragment AdventurePlayFragment on Adventure { id playPublicId publicId thirdPerson actionCount lastAction { ...ActionFragment __typename } ...AdventureControllerFragment ...AudioAdventureControllerFragment ...ReviewAdventureControllerFragment __typename}fragment ActionFragment on Action { id text adventureId undoneAt deletedAt __typename}fragment AdventureControllerFragment on Adventure { id actionLoading error gameState events thirdPerson userId characters { id userId name __typename } ...ActionAdventureControllerFragment ...AlterControllerFragment ...QuestControllerFragment ...RememberControllerFragment ...ShareControllerFragment __typename}fragment ActionAdventureControllerFragment on Adventure { id publicId actionCount choices error mode thirdPerson userId worldId characters { id userId name __typename } ...DeathControllerFragment __typename}fragment DeathControllerFragment on Adventure { id publicId mode died __typename}fragment AlterControllerFragment on Adventure { id publicId mode __typename}fragment QuestControllerFragment on Adventure { id publicId lastAction { id text __typename } quests { id text completed active actionGainedId actionCompletedId __typename } __typename}fragment RememberControllerFragment on Adventure { id memory authorsNote __typename}fragment ShareControllerFragment on Adventure { id userId thirdPerson playPublicId characters { id userId name __typename } __typename}fragment AudioAdventureControllerFragment on Adventure { id music lastAction { id text __typename } __typename}fragment ReviewAdventureControllerFragment on Adventure { id actionCount __typename}","auth":{"token":"hello"}}}'
+        )
 
         # send message
-        await ws.send('{"id":"2","type":"start","payload":{"variables":{"input":{"publicId":"' + str(adventure_id) + '","type":"' + command + '","text":' + json.dumps(text) + ',"choicesMode":false}},"extensions":{},"query":"mutation ($input: ActionInput) { addAction(input: $input) { time message __typename }}","auth":{"token":"hello"}}}')
+        await ws.send(
+            '{"id":"2","type":"start","payload":{"variables":{"input":{"publicId":"'
+            + str(adventure_id)
+            + '","type":"'
+            + command
+            + '","text":'
+            + json.dumps(text)
+            + ',"choicesMode":false}},"extensions":{},"query":"mutation ($input: ActionInput) { addAction(input: $input) { time message __typename }}","auth":{"token":"hello"}}}'
+        )
 
         # listen until a response is returned
         while True:
             resp = await ws.recv()
-            print('Message received during next():')
+            print("Message received during next():")
             print(resp)
             json_payload = json.loads(resp)
 
             if json_payload["type"] == "data" and json_payload["id"] == "2":
-                error_message = Turry.get_deeply_nested(json_payload, ["payload", "data", "addAction", "message"])
+                error_message = Turry.get_deeply_nested(
+                    json_payload, ["payload", "data", "addAction", "message"]
+                )
                 if error_message is not None:
-                    print("Got a message back after sending an action... assuming it's an error...")
+                    print(
+                        "Got a message back after sending an action... assuming it's an error..."
+                    )
                     print(error_message)
                     await ws.close()
                     return error_message, last_seen_id
 
             if json_payload["type"] == "data" and json_payload["id"] == "1":
-                action_response = Turry.get_deeply_nested(json_payload, ["payload", "data", "adventureUpdated", "lastAction"])
-                if (action_response is not None and "text" in action_response and "id" in action_response and
-                        action_response["id"] != last_seen_id):
+                action_response = Turry.get_deeply_nested(
+                    json_payload, ["payload", "data", "adventureUpdated", "lastAction"]
+                )
+                if (
+                    action_response is not None
+                    and "text" in action_response
+                    and "id" in action_response
+                    and action_response["id"] != last_seen_id
+                ):
                     await ws.close()
                     return action_response["text"], action_response["id"]
 
@@ -153,11 +209,15 @@ class Turry(Module):
         print("adventure id:" + adventure_id)
 
         # subscribe to current adventure
-        await ws.send('{"id":"1","type":"start","payload":{"variables":{"input":{"publicId":"' + str(adventure_id) + '","type":"undo","choicesMode":false}},"extensions":{},"query":"mutation ($input: ActionInput) { addAction(input: $input) { time message __typename }}","auth":{"token":"hello"}}}')
+        await ws.send(
+            '{"id":"1","type":"start","payload":{"variables":{"input":{"publicId":"'
+            + str(adventure_id)
+            + '","type":"undo","choicesMode":false}},"extensions":{},"query":"mutation ($input: ActionInput) { addAction(input: $input) { time message __typename }}","auth":{"token":"hello"}}}'
+        )
 
         while True:
             resp = await ws.recv()
-            print('Message received during revert():')
+            print("Message received during revert():")
             print(resp)
             json_payload = json.loads(resp)
 
@@ -178,6 +238,3 @@ class Turry(Module):
                 return None
             current_value = current_value[key]
         return current_value
-
-
-
