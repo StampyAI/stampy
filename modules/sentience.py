2cb7d8616b7c775a45245cb6ae23a8123da09018
Chris Canal <chriscanal4@gmail.com>
2021-04-09 12:13:52 -0500

    updated sentience to avoid error in testing server

--- modules/sentience.py
+++ modules/sentience.py
@@ -5,5 +5,8 @@ class Sentience(Module):
     def process_message(self, message, client=None):
         return 0.0000001, "I don't understand"
 
+    def can_process_message(self, message, client=None):
+        return 0.0000001, "I don't understand"
+
 
 sentience = Sentience()
