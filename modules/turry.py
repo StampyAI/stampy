eaf0a79fd61b9afff5242ad6411089d07f33dd29
= <james.mccuen1@gmail.com>
2021-04-09 12:17:12 -0700

    Added better commands / regex for the AI dungeon actions

--- modules/turry.py
+++ modules/turry.py
@@ -24,95 +24,47 @@ class Turry(Module):
         self.ws = None
         self.adventure_id = "a314f913-7b9e-4777-a48a-b8432e0dceaa"
         self.last_seen_id = None
+
+        self.re_do = re.compile(r"(\*|_)((.*\n)*.*)(\*|_)")
+        self.re_say = re.compile(r"([^#]+#\d\d\d\d )?> (.*)")
+        self.re_story = re.compile(r"^```([\s\S]*?)```$")
+
         atexit.register(self.exit_handler)
         return
 
     def can_process_message(self, message, client=None):
-        if hasattr(message.channel, "name") and (
-            message.channel.name == "seed" or message.channel.name == "seed-test"
-        ):
-            return 7, ""
+        if hasattr(message.channel, "name") and message.channel.name == "seed":
+            command, text = self.get_message_type(message)
+            if command:
+                return 7, ""
         return 0, ""
 
     async def process_message(self, message, client=None):
-
-        if hasattr(message.channel, "name") and (
-            message.channel.name == "seed" or message.channel.name == "seed-test"
-        ):
-            text = message.clean_content
-            match = re.search("^(![^\s]*)(\n|\s|\r|$)", text)
-            command = match.group(1)
-            prompt = text.replace(command + " ", "")
-
-            if not self.adventure_id:
-                await message.channel.trigger_typing()
+        reply = ""
+        if hasattr(message.channel, "name") and message.channel.name == "seed":
+            command, text = self.get_message_type(message)
+            if command:
                 self.ws = await self.get_initialized_websocket()
-                self.adventure_id = await self.process_game_restart()
-
-            if command == "!new":
                 await message.channel.trigger_typing()
-                self.ws = await self.get_initialized_websocket()
-                self.adventure_id = await self.process_game_restart()
-                if self.adventure_id:
-                    response = "New game started: " + self.adventure_id
-            elif command == "!do":
-                await message.channel.trigger_typing()
-                self.ws = await self.get_initialized_websocket()
-                message = await self.process_game_next(
-                    "do", prompt, self.adventure_id, self.last_seen_id
-                )
-                if message:
-                    response = message[0]
-                    self.last_seen_id = message[1]
-            elif command == "!say":
-                await message.channel.trigger_typing()
-                self.ws = await self.get_initialized_websocket()
-                message = await self.process_game_next(
-                    "say", prompt, self.adventure_id, self.last_seen_id
-                )
-                if message:
-                    response = message[0]
-                    self.last_seen_id = message[1]
-            elif command == "!story":
-                await message.channel.trigger_typing()
-                self.ws = await self.get_initialized_websocket()
-                message = await self.process_game_next(
-                    "story", prompt, self.adventure_id, self.last_seen_id
-                )
-                if message:
-                    response = message[0]
-                    self.last_seen_id = message[1]
-            elif command == "!continue":
-                await message.channel.trigger_typing()
-                self.ws = await self.get_initialized_websocket()
-                message = await self.process_game_next(
-                    "start", "", self.adventure_id, self.last_seen_id
-                )
-                response = message[0]
-                self.last_seen_id = message[1]
-            else:
-                return 0, ""
-
-            if response:
-                return 7, response
-        """Handle the message, return a string which is your response.
-        This is an async function so it can interact with the Discord API if it needs to"""
-        return 0, ""
-
-    async def process_reaction_event(
-        self, reaction, user, event_type="REACTION_ADD", client=None
-    ):
-        """event_type can be 'REACTION_ADD' or 'REACTION_REMOVE'
-        Use this to allow modules to handle adding and removing reactions on messages"""
-        return 0, ""
-
-    async def process_raw_reaction_event(self, event, client=None):
-        """event is a discord.RawReactionActionEvent object
-        Use this to allow modules to handle adding and removing reactions on messages"""
+                if command == "do" or command == "say" or command == "story":
+                    response = await self.process_game_next(
+                        "do", text, self.adventure_id, self.last_seen_id
+                    )
+                    if response:
+                        reply = response[0]
+                        self.last_seen_id = response[1]
+                if command == "continue":
+                    response = await self.process_game_next(
+                        "start", "", self.adventure_id, self.last_seen_id
+                    )
+                    reply = response[0]
+                    self.last_seen_id = response[1]
+                if reply:
+                    return 7, reply
         return 0, ""
 
     def __str__(self):
-        return "Turry Module"
+        return "Turry's D&D Module"
 
     async def get_initialized_websocket(self):
         ws = await websockets.connect(self.ws_url, subprotocols=["graphql-ws"])
@@ -161,6 +113,7 @@ class Turry(Module):
             + '"},"extensions":{},"query":"subscription ($publicId: String) { adventureUpdated(publicId: $publicId) { ...AdventurePlayFragment __typename }}fragment AdventurePlayFragment on Adventure { id playPublicId publicId thirdPerson actionCount lastAction { ...ActionFragment __typename } ...AdventureControllerFragment ...AudioAdventureControllerFragment ...ReviewAdventureControllerFragment __typename}fragment ActionFragment on Action { id text adventureId undoneAt deletedAt __typename}fragment AdventureControllerFragment on Adventure { id actionLoading error gameState events thirdPerson userId characters { id userId name __typename } ...ActionAdventureControllerFragment ...AlterControllerFragment ...QuestControllerFragment ...RememberControllerFragment ...ShareControllerFragment __typename}fragment ActionAdventureControllerFragment on Adventure { id publicId actionCount choices error mode thirdPerson userId worldId characters { id userId name __typename } ...DeathControllerFragment __typename}fragment DeathControllerFragment on Adventure { id publicId mode died __typename}fragment AlterControllerFragment on Adventure { id publicId mode __typename}fragment QuestControllerFragment on Adventure { id publicId lastAction { id text __typename } quests { id text completed active actionGainedId actionCompletedId __typename } __typename}fragment RememberControllerFragment on Adventure { id memory authorsNote __typename}fragment ShareControllerFragment on Adventure { id userId thirdPerson playPublicId characters { id userId name __typename } __typename}fragment AudioAdventureControllerFragment on Adventure { id music lastAction { id text __typename } __typename}fragment ReviewAdventureControllerFragment on Adventure { id actionCount __typename}","auth":{"token":"hello"}}}'
         )
 
+
         # send message
         await ws.send(
             '{"id":"2","type":"start","payload":{"variables":{"input":{"publicId":"'
@@ -204,24 +157,33 @@ class Turry(Module):
                     await ws.close()
                     return action_response["text"], action_response["id"]
 
-    async def process_game_revert(self, adventure_id):
-        ws = await self.get_initialized_websocket()
-        print("adventure id:" + adventure_id)
+    async def process_game_command(self, adventure_id, command):
+        ws = self.ws
 
-        # subscribe to current adventure
         await ws.send(
             '{"id":"1","type":"start","payload":{"variables":{"input":{"publicId":"'
             + str(adventure_id)
-            + '","type":"undo","choicesMode":false}},"extensions":{},"query":"mutation ($input: ActionInput) { addAction(input: $input) { time message __typename }}","auth":{"token":"hello"}}}'
+            + '","type":"' + command + '","choicesMode":false}},"extensions":{},"query":"mutation ($input: ActionInput) { addAction(input: $input) { time message __typename }}","auth":{"token":"hello"}}}'
         )
 
         while True:
             resp = await ws.recv()
-            print("Message received during revert():")
             print(resp)
             json_payload = json.loads(resp)
 
+            if json_payload["type"] == "data" and json_payload["id"] == "1":
+                action_response = Turry.get_deeply_nested(
+                    json_payload, ["payload", "data", "adventureUpdated", "lastAction"]
+                )
+                if (
+                    action_response is not None
+                    and "text" in action_response
+                    and "id" in action_response
+                ):
+                    await ws.close()
+                    return action_response["text"], action_response["id"]
             if json_payload["type"] == "complete":
+                await ws.close()
                 return True
 
     def exit_handler(self):
@@ -238,3 +200,43 @@ class Turry(Module):
                 return None
             current_value = current_value[key]
         return current_value
+
+    def get_message_type(self, message):
+
+        content, command, text = message.clean_content, None, None
+
+        if content.startswith(">"):
+            command = "say"
+            text = self.extract_quote(content)
+        if re.match(self.re_story, content):
+            command = "story"
+            text = re.match(self.re_story, content).group(1)
+        if re.match(self.re_do, content):
+            command = "do"
+            text = re.match(self.re_do, content).group(2).lower()
+        if self.is_at_me(message):
+            content = self.is_at_me(message)
+            if content.lower().startswith("retcon that to:"):
+                command = "alter"
+                text = self.extract_quote(text)
+            if content.lower() == "i guess that did happen":
+                command = "redo"
+            if content.lower() == "that isn't what happened":
+                command = "retry"
+            if content.lower() == "what happened next?":
+                command = "continue"
+
+        return command, text
+
+    @staticmethod
+    def extract_quote(text):
+        """Pull the quote text out of the message"""
+        lines = text.split("\n")
+        message = ""
+        for line in lines:
+            # pull out the quote syntax "> " and a user if there is one
+            match = re.match(r"([^#]+#\d\d\d\d )?> (.*)", line)
+            if match:
+                message += match.group(2) + " "
+
+        return message
