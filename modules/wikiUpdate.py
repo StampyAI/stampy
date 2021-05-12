17cd3c9b4eeae265b39c355320610df82ad9b6d4
AugustusCeasar <agustinplazareino@gmail.com>
2021-05-12 20:12:49 +0200

    wikiUpdate module streamlined so commands can be added more easily

--- modules/wikiUpdate.py
+++ modules/wikiUpdate.py
@@ -1,5 +1,7 @@
 from modules.module import Module
 import re
+from functools import partial
+
 
 class WikiUpdate(Module):
     def __str__(self):
@@ -7,32 +9,48 @@ class WikiUpdate(Module):
 
     def __init__(self):
         Module.__init__(self)
-        self.command_not_a_question_re = re.compile(
-            r"((Stampy )?[Tt]ag (that|this)( as)?|[Tt](hat|his) is|[Tt]hat'?s|[Tt](his|at) (question|comment) is) (not (a )?question)(,? [Ss]tampy)?"
-        )
-        self.command_for_rob_re = re.compile(
-            r"((Stampy )?[Tt]ag (that|this)( as)?|[Tt](hat|his) is|([Tt]hat'?s)|[Tt](his|at) (question|comment) is) (for rob)(,? [Ss]tampy)?"
-        )
-        self.command_rejected = re.compile(
-            r"(Stampy )?((([Tt]ag (that|this)( as)?|[Tt](hat|his) is|[Tt]hat'?s|[Tt](his|at) (question|comment) is) rejected)|reject (that|this))(,? [Ss]tampy)?"
-        )
-        self.command = None
 
-# "Tag that as not a question", or "that's not question" etc
-# "tag that as for rob", or "this question is for rob" etc
-# "tag that as rejected" or "reject that"
+        # command_dict contains all the commands, which regex triggers them
+        # and which function [signature (*f)(self, message)] should be triggered when the regex matches
+        # for simple commands that change a single property, get_simple_property_change_partial_function can be used
+        # for more complicated commands, command should be a reference to a function or lambda function
+        # [if a command is particularly important put it at the start of the dict, since matches are evaluated in order]
+        self.command_dict = {
+            "Not A Question": {
+                "re": re.compile(
+                    r"((Stampy )?[Tt]ag (that|this)( as)?|[Tt](hat|his) is|[Tt]hat'?s|[Tt](his|at) (question|comment) is) (not (a )?question)(,? [Ss]tampy)?"
+                ),
+                "command": self.get_simple_property_change_partial_function(
+                    "notquestion", "Yes"
+                ),
+            },
+            "For Rob": {
+                "re": re.compile(
+                    r"((Stampy )?[Tt]ag (that|this)( as)?|[Tt](hat|his) is|([Tt]hat'?s)|[Tt](his|at) (question|comment) is) (for rob)(,? [Ss]tampy)?"
+                ),
+                "command": self.get_simple_property_change_partial_function(
+                    "forrob", "Yes"
+                ),
+            },
+            "Rejected": {
+                "re": re.compile(
+                    r"(Stampy )?((([Tt]ag (that|this)( as)?|[Tt](hat|his) is|[Tt]hat'?s|[Tt](his|at) (question|comment) is) rejected)|reject (that|this))(,? [Ss]tampy)?"
+                ),
+                "command": self.get_simple_property_change_partial_function(
+                    "reviewed", "0"
+                ),
+            },
+        }
+
+        self.command = None
 
     def can_process_message(self, message, client=None):
         """From the Module() Interface. Is this a message we can process?"""
 
-        if re.match(self.command_not_a_question_re, message.clean_content):
-            self.command = self.process_not_a_qestion
-        elif re.match(self.command_for_rob_re, message.clean_content):
-            self.command = self.process_for_rob
-        elif re.match(self.command_rejected, message.clean_content):
-            self.command = self.process_rejected
-        else:
-            self.command = None
+        for k, v in self.command_dict.items():
+            if v["re"].match(message.clean_content):
+                self.command = v["command"]
+                break
 
         if self.command:
             return 8, ""
@@ -45,32 +63,34 @@ class WikiUpdate(Module):
             return await self.command(message)
         return 0, ""
 
-    async def process_not_a_qestion(self, message):
+    def get_simple_property_change_partial_function(self, property_name, new_value):
+        """returns a function that when executed given a specific message performs the appropriate property change"""
+        return partial(
+            self.process_simple_property_change,
+            property_name=property_name,
+            new_value=new_value,
+        )
+
+    async def process_simple_property_change(self, message, property_name, new_value):
+        """Changes just a single property from the referenced question (or the last question stampy posted)"""
         wiki_title = await self.get_wiki_title(message)
         if not wiki_title:
             return 6, "It is not clear what question you are referring to"
 
-        self.utils.wiki.set_question_property(wiki_title, "notquestion", "Yes")
-        return 8, "Processing not a question request for " + wiki_title
+        self.utils.wiki.set_question_property(wiki_title, property_name, new_value)
 
-    async def process_for_rob(self, message):
-        wiki_title = await self.get_wiki_title(message)
-        if not wiki_title:
-            return 6, "It is not clear what question you are referring to"
-
-        self.utils.wiki.set_question_property(wiki_title, "forrob", "Yes")
-        return 8, "Processing for rob request" + wiki_title
-
-    async def process_rejected(self, message):
-        wiki_title = await self.get_wiki_title(message)
-        if not wiki_title:
-            return 6, "It is not clear what question you are referring to"
-
-        self.utils.wiki.set_question_property(wiki_title, "reviewed", "0")
-
-        return 8, "Processing rejected request" + wiki_title
+        return (
+            8,
+            "Processing "
+            + property_name
+            + "="
+            + new_value
+            + " request on "
+            + wiki_title,
+        )
 
     async def get_wiki_title(self, message):
+        """parses the message reference to get the wiki title of the referenced question"""
         if message.reference:
             # if this message is a reply
             reference = await message.channel.fetch_message(
@@ -82,7 +102,8 @@ class WikiUpdate(Module):
             question_user = "Unknown User"
             if reference_text:
                 match = re.match(
-                    r"YouTube user (.*?)( just)? asked (a|this) question", reference_text
+                    r"YouTube user (.*?)( just)? asked (a|this) question",
+                    reference_text,
                 )
             if match:
                 question_user = match.group(1)  # YouTube user (.*) asked this question
