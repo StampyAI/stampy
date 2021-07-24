09defa518225e12505f9c1786494149cf3d84ad8
robertskmiles <github@robertskmiles.com>
2021-07-24 22:35:27 +0100

    make dummy sentience module only reply when stampy is adressed

--- modules/sentience.py
+++ modules/sentience.py
@@ -3,7 +3,10 @@ from modules.module import Module, Response
 
 class Sentience(Module):
     def process_message(self, message, client=None):
-        return Response(confidence=0.0000001, text="I don't understand")
+        if self.is_at_me(message):
+            return Response(confidence=0.0000001, text="I don't understand")
+        else:
+            return Response()
 
     def __str__(self):
         return "Sentience"
