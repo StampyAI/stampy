619ef546a6061b234e0d8c27b696f654c18a281f
Ulisse Mini <ulisse.mini@gmail.com>
2021-07-25 18:08:55 -0400

    Use builtin os.environ[key] for required keys
    The previous version with required_env_keys and check_environment in
    utilities was needlessly complex and lead to bugs such as the ordering
    of load_dotenv which was fixed in a previous commit.

--- config.py
+++ config.py
@@ -16,42 +16,29 @@ youtube_testing_thread_url = "https://www.youtube.com/watch?v=vuYtSDMBLtQ&lc=Ugx
 
 bot_dev_channel_id = {"production": 808138366330994688, "development": 803448149946662923}
 
-discord_token_env_variable = "DISCORD_TOKEN"
-discord_guild_env_variable = "DISCORD_GUILD"
-youtube_api_key_env_variable = "YOUTUBE_API_KEY"
-database_path_env_variable = "DATABASE_PATH"
-wiki_password_path_env_variable = "WIKI_BOT_PASSWORD"
-environment_type_env_variable = "ENVIRONMENT_TYPE"
-openai_env_variable = "OPENAI_API_KEY"
-
 prod_local_path = "/home/rob/stampy.local"
 
 admin_usernames = ["robertskmiles", "sudonym"]
 
 
-discord_token = os.getenv(discord_token_env_variable)
-discord_guild = os.getenv(discord_guild_env_variable)
-youtube_api_key = os.getenv(youtube_api_key_env_variable)
-database_path = os.getenv(database_path_env_variable)
-wiki_password = os.getenv(wiki_password_path_env_variable)
-openai_api_key = os.getenv(openai_env_variable, "null")
+discord_token = os.environ["DISCORD_TOKEN"]
+discord_guild = os.environ["DISCORD_GUILD"]
+youtube_api_key = os.environ["YOUTUBE_API_KEY"]
+database_path = os.environ["DATABASE_PATH"]
+wiki_password = os.environ["WIKI_BOT_PASSWORD"]
+openai_api_key = os.getenv("OPENAI_API_KEY", "null")
 
 wiki_config = {"uri": "https://stampy.ai/w/api.php", "user": "Stampy@stampy", "password": wiki_password}
 
-ENVIRONMENT_TYPE = os.getenv(environment_type_env_variable)
+ENVIRONMENT_TYPE = os.environ["ENVIRONMENT_TYPE"]
 acceptable_environment_types = ("production", "development")
+assert ENVIRONMENT_TYPE in acceptable_environment_types, \
+    f'ENVIRONMENT_TYPE {ENVIRONMENT_TYPE} is not in {acceptable_environment_types}'
 
 if ENVIRONMENT_TYPE == "development":
     rob_miles_youtube_channel_id = "UCDvKrlpIXM0BGYLD2jjLGvg"
     stampy_youtube_channel_id = "DvKrlpIXM0BGYLD2jjLGvg"
 
-required_environment_variables = [
-    environment_type_env_variable,
-    discord_token_env_variable,
-    discord_guild_env_variable,
-    database_path_env_variable,
-    wiki_password_path_env_variable,
-]
 
 stampy_control_channel_names = [
     "bot-dev-priv",
