f27e30334d82933269109ee1ee37bd4e9867dc5f
Ulisse Mini <ulisse.mini@gmail.com>
2021-07-26 13:38:38 -0400

    Custom getenv function for nicer error messages

--- config.py
+++ config.py
@@ -2,6 +2,17 @@ import os
 import dotenv
 dotenv.load_dotenv()
 
+def getenv(env_var, default=None):
+    """
+    Get an environment variable with a default,
+    raise an exception if the environment variable isn't set and no default is provided
+    """
+    value = os.getenv(env_var, default)
+    if value is None:
+        raise Exception(f"Environment Variable '{env_var}' not set and no default provided")
+    return value
+
+
 maximum_recursion_depth = 30
 subs_dir = "./database/subs"
 youtube_api_service_name = "youtube"
@@ -11,7 +22,7 @@ stampy_id = "736241264856662038"
 plex_id = "756254556811165756"
 god_id = "0"
 
-ENVIRONMENT_TYPE = os.environ["ENVIRONMENT_TYPE"]
+ENVIRONMENT_TYPE = getenv("ENVIRONMENT_TYPE")
 acceptable_environment_types = ("production", "development")
 assert ENVIRONMENT_TYPE in acceptable_environment_types, f'ENVIRONMENT_TYPE {ENVIRONMENT_TYPE} is not in {acceptable_environment_types}'
 
@@ -31,12 +42,12 @@ prod_local_path = "/home/rob/stampy.local"
 admin_usernames = ["robertskmiles", "sudonym"]
 
 
-discord_token = os.environ["DISCORD_TOKEN"]
-discord_guild = os.environ["DISCORD_GUILD"]
-youtube_api_key = os.environ["YOUTUBE_API_KEY"]
-database_path = os.environ["DATABASE_PATH"]
-wiki_password = os.environ["WIKI_BOT_PASSWORD"]
-openai_api_key = os.getenv("OPENAI_API_KEY", "null")
+discord_token = getenv("DISCORD_TOKEN")
+discord_guild = getenv("DISCORD_GUILD")
+youtube_api_key = getenv("YOUTUBE_API_KEY")
+database_path = getenv("DATABASE_PATH")
+wiki_password = getenv("WIKI_BOT_PASSWORD")
+openai_api_key = getenv("OPENAI_API_KEY", default="null")
 
 wiki_config = {"uri": "https://stampy.ai/w/api.php", "user": "Stampy@stampy", "password": wiki_password}
 
