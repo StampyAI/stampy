83db4e92425f97c06f4580eee1a3661437fd9ca6
Chris Canal <chriscanal4@gmail.com>
2021-07-28 20:53:42 -0500

    added test for sentience module

--- modules/sentience.py
+++ modules/sentience.py
@@ -2,11 +2,22 @@ from modules.module import Module, Response
 
 
 class Sentience(Module):
+    CONFUSED_RESPONSE = "I don't understand"
+
     def process_message(self, message, client=None):
-        return Response(confidence=0.0000001, text="I don't understand")
+        return Response(confidence=0.0000001, text=self.CONFUSED_RESPONSE)
 
     def __str__(self):
         return "Sentience"
+
+    @property
+    def test_cases(self):
+        return [
+            self.create_integration_test(
+                question="If I asked you what 2+2 was and you answered incorrectly what would you have said?",
+                expected_response=self.CONFUSED_RESPONSE,
+            )
+        ]
 
 
 sentience = Sentience()
