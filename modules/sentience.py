3aa8758c94db157e545e37d7b4d95c8c1ae52ecd
robertskmiles <github@robertskmiles.com>
2021-08-10 21:52:27 +0100

    make dummy sentience module stfu unless adressed

--- modules/sentience.py
+++ modules/sentience.py
@@ -4,7 +4,10 @@ from modules.module import Module, Response
 
 class Sentience(Module):
     def process_message(self, message, client=None):
-        return Response(confidence=3, text=CONFUSED_RESPONSE)
+        if self.is_at_me(message):
+            return Response(confidence=3, text=CONFUSED_RESPONSE)
+        else:
+            Response()
 
     def __str__(self):
         return "Sentience"
@@ -13,7 +16,7 @@ class Sentience(Module):
     def test_cases(self):
         return [
             self.create_integration_test(
-                question="If I asked you what 2+2 was and you answered incorrectly what would you have said?",
+                question="Stampy, if I asked you what 2+2 was and you answered incorrectly what would you have said?",
                 expected_response=CONFUSED_RESPONSE,
             )
         ]
