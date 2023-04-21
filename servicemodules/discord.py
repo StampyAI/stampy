5aaa417f372c38bcfdc4cbc80cb6fb622ad0efce
Nowak <jakub.k.nowak@protonmail.com>
2023-04-21 15:37:52 +0200

    Adding a test for channels in discordConstants on discord client init

--- servicemodules/discord.py
+++ servicemodules/discord.py
@@ -25,7 +25,8 @@ from config import (
     TEST_RESPONSE_PREFIX,
     maximum_recursion_depth,
 )
-from servicemodules.discordConstants import stampy_dev_priv_channel_id, automatic_question_channel_id
+from servicemodules import discordConstants
+from servicemodules.serviceConstants import Services, openai_channel_ids
 from api.youtube import YoutubeAPI
 
 log = get_logger()
@@ -69,9 +70,11 @@ class DiscordHandler:
 
             log.info(self.class_name, msg="found a guild named '%s' with id '%s'" % (guild.name, guild.id))
 
+            self.test_channel_constants()
+
             members = "\n - " + "\n - ".join([member.name for member in guild.members])
             log.info(self.class_name, guild_members=members)
-            await self.utils.client.get_channel(int(stampy_dev_priv_channel_id)).send(
+            await self.utils.client.get_channel(int(discordConstants.stampy_dev_priv_channel_id)).send(
                 f"I just (re)started {get_git_branch_info()}!"
             )
 
@@ -108,7 +111,7 @@ class DiscordHandler:
                 message_content=message.content,
             )
 
-            if hasattr(message.channel, "id") and str(message.channel.id) == automatic_question_channel_id:
+            if hasattr(message.channel, "id") and str(message.channel.id) == discordConstants.automatic_question_channel_id:
                 log.info(self.class_name, msg="the latest general discord channel message was not from stampy")
                 self.utils.last_message_was_youtube_question = False
 
@@ -298,3 +301,26 @@ class DiscordHandler:
         t.name = "Discord Thread"
         t.start()
         return t
+
+    def test_channel_constants(self):
+        extra_channel_ids = (discordConstants.archive_category_id,
+                             discordConstants.bot_owner_dms_id,
+                             discordConstants.editing_channel_id,
+                             discordConstants.introductions_channel_id,
+                             discordConstants.memes_channel_id,
+                             discordConstants.off_topic_channel_id,
+                             discordConstants.stampy_dev_channel_id,
+                             discordConstants.stampy_dev_priv_channel_id,
+                             discordConstants.stampy_error_log_channel_id,
+                             discordConstants.test_channel_id,
+                             discordConstants.voice_channel_id,
+                             discordConstants.voice_context_channel_id,
+                             discordConstants.welcome_channel_id,
+                             discordConstants.wiki_channel_id,
+                             discordConstants.wiki_feed_channel_id)
+
+        for channel_ids_list in (extra_channel_ids, openai_channel_ids[Services.DISCORD]):
+            for channel_id in channel_ids_list:
+                if int(channel_id) > 0 and self.utils.client.get_channel(int(channel_id)) is None:
+                    log.warning(self.class_name, msg=f"Could not find a channel with id {channel_id}")
+
