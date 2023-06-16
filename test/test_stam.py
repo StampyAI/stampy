a6c129054f3116852ae3c4efcb9714fa92d292c6
Mateusz Bagiński <m.baginski@optimumpareto.com>
2023-06-16 10:44:52 +0200

    removed breakpoint

--- test/test_stam.py
+++ test/test_stam.py
@@ -11,11 +11,10 @@ class TestStam(TestCase):
         unavailable_module_names = Utilities.get_instance().unavailable_module_names
         enabled_modules_count = len(enabled_modules)
         found_modules_count = len(modules) + len(unavailable_module_names)
-        
+
         enabled_msg = sorted(enabled_modules, key=str.casefold)
         modules_msg = sorted(modules, key=str.casefold)
         unavailable_msg = sorted(unavailable_module_names, key=str.casefold)
-        breakpoint()
         self.assertEqual(
             enabled_modules_count,
             found_modules_count,
